a267519bb50d5cf459b1d143ce83dbc18e28ceba
liangfutang <tanglf@kalman-navigation.com>
2024-10-23 19:34:22 +0800

    1. 结构初始化项目

--- src/renderer/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-const app = createApp(App).mount('#app')
-
-app.use(ElementPlus)
-app.mount('#app')
