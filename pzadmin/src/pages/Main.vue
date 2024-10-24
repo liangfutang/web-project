16f0e49bf45dd35c6b0d79e7e2e9d20964646600
liangfutang <tanglf@kalman-navigation.com>
2024-10-24 13:52:45 +0800

    1. 按需引入elementplus

--- pzadmin/src/pages/Main.vue
+++ pzadmin/src/pages/Main.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="main">
-    main
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
   </div>
 </template>
 
-<script>
+<script >
+
 export default {
 
 }
