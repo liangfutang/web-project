0993e9afb8f7027a0feb302c0c1c77db717d3f7c
liangfutang <tanglf@kalman-navigation.com>
2025-03-07 17:17:09 +0800

    1. 添加内嵌页面

---
+++ pzadmin/src/views/vppz/order/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    陪诊订单
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pzadmin/src/views/vppz/staff/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    陪护管理
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
