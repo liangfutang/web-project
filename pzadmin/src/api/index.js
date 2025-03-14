79570b79aed0f3f9da19028b2cf49ec3152d3a8a
liangfutang <tanglf@kalman-navigation.com>
2025-03-14 14:45:45 +0800

    1. 实现新增权限菜单的对话框基本样式

--- pzadmin/src/api/index.js
+++ pzadmin/src/api/index.js
@@ -12,3 +12,21 @@ export const userAuthentication = (data) =>{
 export const login = (data) =>{
     return request.post('/login',data)
 }
+//账号权限管理列表
+export const authAdmin = (params) =>{
+    return request.get('/auth/admin',{params})
+}
+//菜单权限数据
+export const userGetMenu = () =>{
+    return request.get('/user/getmenu')
+}
+
+//菜单权限修改   没有data会报参数不对，检查string类型  
+export const userSetMenu = (data) =>{
+    return request.post('/user/setmenu',data)
+}
+
+//菜单权限列表
+export const menuList = (params) =>{
+    return request.get('/menu/list',{params})
+}
--- pzadmin/src/views/auth/menuManage/index.vue
+++ pzadmin/src/views/auth/menuManage/index.vue
@@ -1,11 +1,65 @@
 <template>
   <div>
-    菜单管理
+    <el-button @click="dialogVisible = true" type="primary" size="small">新增</el-button>
+    
+    <el-dialog
+      v-model="dialogVisible"
+      title="添加权限"
+      width="500"
+      :before-close="handleClose"
+    >
+      <el-form :rules="formRules" :model="formData">
+        <el-form-item prop="name" label="名称">
+          <el-input v-model="formData.name"></el-input>
+        </el-form-item>
+        <el-form-item prop="name" label="权限">
+          <el-tree
+            style="max-width: 600px"
+            show-checkbox
+            :data="permissionData"
+          />
+        </el-form-item>
+      </el-form>
+      
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="dialogVisible = false">
+            Confirm
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script setup>
+import { ref, reactive, onMounted } from 'vue'
+import { userGetMenu } from '../../../api/index'
 
+onMounted(() => {
+  //菜单数据
+  userGetMenu().then(({ data }) => {
+    console.log(data);
+    permissionData.value = data.data;
+  });
+});
+
+const dialogVisible = ref(false)
+const permissionData = ref([])
+
+const formData = reactive({
+  name: '',
+})
+const formRules = reactive({
+  name: [
+    { required: true, message: '请输入权限名称', trigger: 'blur' },
+  ],
+})
+
+const handleClose = () => {
+  dialogVisible.value = false
+}
 </script>
 
 <style scoped>
