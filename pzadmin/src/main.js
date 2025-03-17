cf264701950a8a211dff4ce88854a1fa7a3c7a59
liangfutang <tanglf@kalman-navigation.com>
2025-03-17 10:19:46 +0800

    1. 刷新页面后添加动态路由，解决刷新后空白的问题

--- pzadmin/src/main.js
+++ pzadmin/src/main.js
@@ -5,6 +5,14 @@ import router from './router'
 import store from './store'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
+// 刷新后的动态路由添加
+if (localStorage.getItem('pz_token')) {
+    store.commit('dynamicMenu', store.state.menu.routerList)
+    store.state.menu.routerList.forEach(route => {
+      router.addRoute('main', route);
+    });
+}
+
 const app = createApp(App)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
