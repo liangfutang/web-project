e73bfb9fe097c117f6f3fa6ca1d6257f3a5faca2
liangfutang <tanglf@kalman-navigation.com>
2025-03-18 16:08:48 +0800

    1. 陪护管理添加panelhead显示

--- pzadmin/src/views/vppz/staff/index.vue
+++ pzadmin/src/views/vppz/staff/index.vue
@@ -1,13 +1,44 @@
 <template>
   <div>
-    陪护管理
+    <panel-head :route="route" />
   </div>
 </template>
 
 <script setup>
+import PanelHead from '../../../components/panelHead.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
 
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.btns {
+  padding: 10px 0 10px 10px;
+  border: 0px;
+}
+.image-list {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  .img-box {
+    position: relative;
+    .select {
+      position: absolute;
+      left: 0px;
+      top: 0px;
+      width: 24px;
+      height: 24px;
+      background-color: #67c23a;
+      z-index: 999;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+  .el-image {
+    margin-right: 10px;
+    margin-bottom: 10px;
+  }
+}
 </style>
