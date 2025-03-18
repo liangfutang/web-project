172fe274061622e3a7b7b84d418bc1e6ef76cd64
liangfutang <tanglf@kalman-navigation.com>
2025-03-18 17:44:08 +0800

    1. 陪护管理的列表显示

--- pzadmin/src/views/vppz/staff/index.vue
+++ pzadmin/src/views/vppz/staff/index.vue
@@ -18,6 +18,33 @@
         </template>
       </el-popconfirm>
     </button>
+
+    <el-table :data="tableData.list" style="width: 100%">
+      <el-table-column prop="id" label="id" />
+      <el-table-column prop="name" label="昵称" />
+      <el-table-column prop="avatar" label="头像" >
+        <template #default="scope">
+          <el-image style="width: 50px;height: 50px;" :src="scope.row.avatar" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="sex" label="性别">
+        <template #default="scope">
+          {{ scope.row.sex === "1" ? '男' : '女' }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="mobile" label="手机号" />
+      <el-table-column prop="active" label="状态">
+        <template #default="scope">
+          <el-tag :type="scope.row.active ? 'success' : 'danger'">{{ scope.row.active ? '生效' : '失效' }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="create_time" label="创建时间" />
+      <el-table-column label="操作">
+        <template #default="scope">
+          <el-button type="primary" @click="open(scope.row)">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -25,14 +52,40 @@
 import PanelHead from '../../../components/panelHead.vue'
 import { useRoute } from 'vue-router'
 import { Plus, Delete, InfoFilled } from '@element-plus/icons-vue'
+import { reactive, onMounted } from 'vue'
+import { dayjs } from 'element-plus'
+import { companionList } from '../../../api/index.js'
+
+onMounted(() => {
+  companionList(paginationData).then(({ data }) => {
+    const { list, total } = data.data
+    list.forEach(item => {
+      item.create_time = dayjs(item.create_time).format('YYYY-MM-DD')
+    });
+    tableData.list = list
+    tableData.total = total
+  })
+})
 
 const route = useRoute()
+const tableData = reactive({
+  list: [],
+  total: 0
+})
+//分页数据
+const paginationData = reactive({
+    pageNum:1,
+    pageSize:10
+})
 
 const confirmEvent = () => {
   console.log('confirm')
 }
 const cancelEvent = () => {
   console.log('cancel')
+}
+const open = (rowData) => {
+  console.log(rowData)
 }
 </script>
 
