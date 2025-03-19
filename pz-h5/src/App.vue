434835d14910262dd13abc1b544071b80ba1cdba
liangfutang <tanglf@kalman-navigation.com>
2025-03-19 15:12:57 +0800

    1. 安装vant

--- pz-h5/src/App.vue
+++ pz-h5/src/App.vue
@@ -3,15 +3,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success">成功按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
+
 </template>
 
 <style scoped>
