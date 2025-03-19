dd39f8e68e9763918960f186777e631da44b7180
liangfutang <tanglf@kalman-navigation.com>
2025-03-19 16:54:47 +0800

    1. 实现标签栏

--- pz-h5/src/pages/Main.vue
+++ pz-h5/src/pages/Main.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>首页111</div>
+  <van-tabbar v-model="active">
+    <van-tabbar-item v-for="(item) in router.options.routes[0].children" :key="item.path" :icon="item.meta.icon" :url="`#/${item.path}`">{{ item.meta.name }}</van-tabbar-item>
+  </van-tabbar>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+onMounted(() => {
+    console.log(router.options);
+    const data = router.options.routes[0]
+    active.value = data.children.findIndex(item => '/' + item.path === route.path)
+})
+
+const active = ref(0);
+const router = useRouter();
+const route = useRoute();
+
 
 </script>
 
