6b0403ba5122b41a7cf163f9b574b2637626c415
liangfutang <tanglf@kalman-navigation.com>
2025-03-25 19:40:03 +0800

    1. 实现我的页面部分功能

--- pz-h5/src/pages/user/index.vue
+++ pz-h5/src/pages/user/index.vue
@@ -5,6 +5,56 @@
         <van-image class="img" width="100" height="100" :src="userInfo.avatar" />
         <div class="text">{{ userInfo.name }}</div>
     </div>
+    <!-- 订单 -->
+    <div class="order">
+        <div class="top">
+            <div class="text1">我的订单</div>
+            <div class="text2">全部</div>
+        </div>
+        <!-- 订单快捷入口 -->
+        <div class="buttom">
+            <div class="item">
+                <van-image @click="goOder(1)" width="40" height="40" src="/images/od_10.png" />
+                <div>待支付</div>
+            </div>
+            <div @click="goOder(2)" class="item">
+                <van-image width="40" height="40" src="/images/od_20.png" />
+                <div>待服务</div>
+            </div>
+            <div @click="goOder(3)" class="item">
+                <van-image width="40" height="40" src="/images/od_30.png" />
+                <div>已完成</div>
+            </div>
+            <div @click="goOder(4)" class="item">
+                <van-image width="40" height="40" src="/images/od_40.png" />
+                <div>已取消</div>
+            </div>
+        </div>
+    </div>
+    <!-- 底部 -->
+    <div class="foot">
+        <div class="foot1">
+            <div class="text1">
+                <van-image width="20" height="20" src="/images/ic_clients.png" />
+                服务对象管理
+            </div>
+            <div class="text2"><van-icon name="arrow" /></div>
+        </div>
+        <div class="foot2">
+            <div class="text1">
+                <van-image width="20" height="20" src="/images/ic_share.png" />
+                分享转发
+            </div>
+            <div class="text2"><van-icon name="arrow" /></div>
+        </div>
+    </div>
+
+    <!-- 登出按钮 -->
+    <van-button type="danger" class="quit" @click="show = true" size="large">退出登录</van-button>
+    <!-- 退出弹窗 -->
+    <van-dialog v-model:show="show" title="提示" @cancel="show = false" @confirm="logout" show-cancel-button>
+        <div class="quit_text">确定退出登录吗？</div>
+    </van-dialog>
   </div>
 </template>
 
@@ -13,10 +63,21 @@ import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
+const show = ref(false)
 // 获取用户信息
 const userInfo = computed(() => {
     return JSON.parse(localStorage.getItem('h5_userInfo')) || {}
 })
+
+// 跳转到订单列表
+const goOder = (active) => {
+    router.push(`/order?active=${active}`)
+}
+const logout = () => {
+    localStorage.removeItem('h5_token')
+    localStorage.removeItem('h5_userInfo')
+    router.push('/login')
+}
 </script>
 
 <style lang="less" scoped>
