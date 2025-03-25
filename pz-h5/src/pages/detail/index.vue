92e01901443926014505ee0b2ceb183e103e85b8
liangfutang <tanglf@kalman-navigation.com>
2025-03-25 19:00:55 +0800

    1. 订单状态描述

--- pz-h5/src/pages/detail/index.vue
+++ pz-h5/src/pages/detail/index.vue
@@ -5,11 +5,48 @@
         订单详情
     </div>
     <status-bar :item="stateMap[detailData.trade_state]" />
+
+    <div class="tips">
+        <!-- 待支付状态 -->
+        <div class="dzf" v-if="detailData.trade_state === '待支付'">
+            <div class="text1">订单待支付</div>
+            <div class="text2">请在
+                <counter :second="second" />内完成支付，超时未支付将自动取消订单。
+            </div>
+            <div class="text3">
+                <van-button type="success" @click="showCode = true">立即支付({{ detailData.price }}元)</van-button>
+            </div>
+        </div>
+        <!-- 待服务状态 -->
+        <div class="dzf" v-if="detailData.trade_state === '待服务'">
+            <div class="text1">正在为您安排服务专员...</div>
+            <div class="text2">请保持手机畅通，稍后将有服务专员与您联系</div>
+        </div>
+        <!-- 已完成状态 -->
+        <div class="dzf" v-if="detailData.trade_state === '已完成'">
+            <div class="text1">服务已完成</div>
+            <div class="text2">感谢您的使用，如有售后问题请联系客服</div>
+        </div>
+        <!-- 已取消状态 -->
+        <div class="dzf" v-if="detailData.trade_state === '已取消'">
+            <div class="text1">订单已取消</div>
+            <div class="text2">期待下次为您服务，如需帮助可咨询客服</div>
+        </div>
+    </div>
+
+    <!-- 支付二维码弹窗 -->
+    <van-dialog :show-confirm-button="false" v-model:show="showCode">
+        <van-icon name="cross" class="close" @click="closeCode" />
+        <div>微信支付</div>
+        <!-- <van-image width="150" height="150" :src="codeImg" /> -->
+        <van-image width="150" height="150" :src="codeImg" />
+        <div>请使用本人微信扫码支付</div>
+    </van-dialog>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, onMounted, getCurrentInstance } from "vue";
+import { ref, reactive, onMounted, getCurrentInstance, computed } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import Qrcode from 'qrcode'
 
@@ -21,11 +58,16 @@ onMounted(async () => {
     })
     console.log(detailData);
 })
+const second = computed(() => {
+    // 订单开始时间+2小时-当前时间
+    return detailData.order_start_time ? detailData.order_start_time + 7200000 - Date.now() : 0
+})
 
 const { proxy:{$api} } = getCurrentInstance();
 const router = useRouter();
 const route = useRoute();
 const codeImg = ref('')
+const showCode = ref(false)
 const detailData = reactive({})
 const stateMap = {
     '待支付': 10,
@@ -36,6 +78,10 @@ const stateMap = {
 
 const goBack = () => {
     router.go(-1)
+}
+const closeCode = () => {
+    showCode.value = false
+    router.push('/order')
 }
 </script>
 
