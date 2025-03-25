5e0efe84a92468a034d948779e3e66d87d3d9358
liangfutang <tanglf@kalman-navigation.com>
2025-03-25 11:09:28 +0800

    1. 订单列表实现

--- pz-h5/src/pages/order/index.vue
+++ pz-h5/src/pages/order/index.vue
@@ -3,27 +3,69 @@
     <van-icon @click="goBack" name="arrow-left" class="header-left" size="40" />
     <div class="header">我的订单</div>
     <van-tabs @click-tab="onClickTab" v-model:active="active">
-        <van-tab title="全部">内容 1</van-tab>
-        <van-tab title="待支付">内容 2</van-tab>
-        <van-tab title="待服务">内容 2</van-tab>
-        <van-tab title="已完成">内容 3</van-tab>
-        <van-tab title="已取消">内容 4</van-tab>
+        <van-tab title="全部" name=""/>
+        <van-tab title="待支付" name="1"/>
+        <van-tab title="待服务" name="2"/>
+        <van-tab title="已完成" name="3"/>
+        <van-tab title="已取消" name="4"/>
     </van-tabs>
+    <!-- 订单列表 -->
+    <van-row v-for="item in order" :key="item.out_trade_no">
+        <van-col span="5">
+            <van-image width="50" height="50" radius="5" :src="item.serviceImg" />
+        </van-col>
+        <van-col span="14">
+            <div class="text1">{{ item.service_name }}</div>
+            <div class="text2">
+                <div>{{ item.hospital_name }}</div>
+                <div>预约时间:{{ item.starttime }}</div>
+            </div>
+        </van-col>
+        <!-- 订单状态 -->
+        <van-col class="text2" :style="{ color: colorMap[item.trade_state] }" span="5">
+            {{ item.trade_state }}
+            <!-- 待支付订单的倒计时 -->
+            <counter :second="item.timer" v-if="item.trade_state === '待支付'" />
+        </van-col>
+    </van-row>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, onMounted } from "vue";
+import { ref, reactive, onMounted, getCurrentInstance } from "vue";
 import { useRouter} from 'vue-router'
+import counter from '../../components/counter.vue'
 
+onMounted(() => {
+    getOrderList()
+})
+
+const { proxy: { $api } } = getCurrentInstance()
 const router = useRouter()
 const active = ref("")
+const order = ref([])
+const colorMap = {
+    '待支付': '#ffa200',
+    '待服务': '#1da6fd',
+    '已完成': '#21c521',
+    // '已取消': '#999999'
+}
 const goBack = () => {
     router.go(-1)
 };
+//获取订单列表
+const getOrderList = async (state) => {
+    const { data } = await $api.orderList({ state })
+    console.log(data);
+    // 倒计时
+    data.data.forEach(item => {
+        item.timer = item.order_start_time + 7200000 - Date.now()
+    })
+    order.value = data.data
+}
 const onClickTab = (item) => {
-    console.log(item);
-};
+    getOrderList(item.name)
+}
 </script>
 
 <style lang="less" scoped>
