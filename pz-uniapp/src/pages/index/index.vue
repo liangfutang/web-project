4211fd2c159850d8c8ddcfc9bc7fbc8938ee62d3
liangfutang <tanglf@kalman-navigation.com>
2025-03-27 10:14:07 +0800

    1.在uniapp中实现一个小的功能模拟

--- pz-uniapp/src/pages/index/index.vue
+++ pz-uniapp/src/pages/index/index.vue
@@ -4,22 +4,45 @@
     <view class="text-area">
       <text class="title">{{ title }} 111</text>
     </view>
+    <button @click="handleCount">点我累加</button>
+    <view class="text-area">
+      <text class="title">水果总数：{{ total }}</text>
+    </view>
+    <view class="text-area" v-for="item in list" :key="item.name">
+      <text class="title">{{ item.name }}:{{ item.value }}</text>
+    </view>
   </view>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      title: 'Hello',
-    }
-  },
-  onLoad() {},
-  methods: {},
+<script setup>
+import { onLoad } from '@dcloudio/uni-app'
+import { ref,reactive,computed } from 'vue'
+
+onLoad(() => {
+  console.log('onLoad加载')
+})
+
+const title = ref('Hello')
+const list = reactive([
+  {name:'橘子', value: 1},
+  {name:'苹果', value: 2},
+  {name:'香蕉', value: 3}
+])
+const total = computed(()=>{
+  let sum = 0
+  list.forEach(item=>{
+    sum += item.value
+  })
+  return list.reduce((tot,cur)=>tot+cur.value, 0)
+})
+const handleCount = ()=>{
+  list.forEach(item=>{
+    item.value++
+  })
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 .content {
   display: flex;
   flex-direction: column;
