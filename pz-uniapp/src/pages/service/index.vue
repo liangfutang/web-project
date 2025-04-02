41d8938e4426e39c7039584ca28b6e0d7ac883e3
liangfutang <tanglf@kalman-navigation.com>
2025-04-02 15:34:51 +0800

    1. 填写服务订单的进度条样式

--- pz-uniapp/src/pages/service/index.vue
+++ pz-uniapp/src/pages/service/index.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="index">服务</div>
+  <view >
+    <view class="od-banner">
+        <image class="od-banner-icon" src="/static/resource/images/od_bg_icon.png" mode="widthFix" />
+        <view class="od-jd od-jd-0">
+            <view class="od-jd-out">
+                <view class="od-jd-in"></view>
+            </view>
+            <view class="vp-flex od-jd-text">
+                <view class="vp-flex_1">
+                    <text class="od-jd-st-0">填写订单</text>
+                </view>
+                <view class="vp-flex_1">
+                    <text class="od-jd-st-10">在线支付</text>
+                </view>
+                <view class="vp-flex_1">
+                    <text class="od-jd-st-20">专人服务</text>
+                </view>
+                <view class="vp-flex_1">
+                    <text class="od-jd-st-30">服务完成</text>
+                </view>
+            </view>
+        </view>
+    </view>
+  </view>
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="less" scoped>
-
+<style scoped>
+@import './index.css'
 </style>
