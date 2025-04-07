e60958565f699b0b5994fbb592a4302fbbfc846e
liangfutang <tanglf@kalman-navigation.com>
2025-04-07 17:28:13 +0800

    1. 订单详情页面

---
+++ pz-uniapp/src/pages/order/server.vue
@@ -0,0 +1,41 @@
+<template>  
+  <view class="container">  
+    <view class="title">联系客服</view>  
+    <view class="content">  
+      <text>请在这里填写您的联系方式和问题，我们将尽快回复。</text>  
+      <!-- 可以添加表单、输入框等组件来收集用户信息 -->  
+      <input type="text" placeholder="请输入您的姓名" />  
+      <input type="text" placeholder="请输入您的联系方式" />  
+      <textarea placeholder="请输入您的问题或反馈"></textarea>  
+      <button @click="submitFeedback">提交</button>  
+    </view>  
+  </view>  
+</template>  
+  
+<script>  
+export default {  
+  methods: {  
+    submitFeedback() {  
+	  //无该功能API接口
+      uni.showToast({  
+        title: '反馈已提交',  
+        icon: 'success'  
+      });  
+    }  
+  }  
+}  
+</script>  
+  
+<style>
+.container {  
+  padding: 40rpx;  
+}  
+.title {  
+  font-size: 40rpx;  
+  font-weight: bold;  
+  margin-bottom: 30rpx;  
+}  
+.content {  
+  margin-top: 20rpx;  
+}  
+</style>
