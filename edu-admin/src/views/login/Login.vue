102d25c921449b8b9a2ad0e9fd087895065aa616
liangfutang <tanglf@kalman-navigation.com>
2025-04-09 16:37:16 +0800

    1. 登录页基本样式

--- edu-admin/src/views/login/Login.vue
+++ edu-admin/src/views/login/Login.vue
@@ -1,16 +1,43 @@
 <template>
-  <div>
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </div>
+  <el-row>
+    <el-card style="max-width: 480px">
+      <template #header>
+        <div class="card-header">
+          <span>登录</span>
+        </div>
+      </template>
+      
+      <el-form ref="loginFormRef">
+        <el-form-item label="">
+          <el-input placeholder="用户名" />
+        </el-form-item>
+        <el-form-item label="">
+          <el-input placeholder="密码" type="password" />
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" style="width: 100%" @click="submitForm(loginFormRef)">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </el-row>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+const loginFormRef = ref()
+
+const submitForm = (formRef) => {
+  formRef.value.validate((valid) => {
+    if (valid) {
+      console.log('submit!');
+    } else {
+      console.log('error submit!!');
+      return false;
+    }
+  });
+}
 </script>
 
 <style lang="less" scoped>
